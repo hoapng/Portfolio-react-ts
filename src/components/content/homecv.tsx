a97dbe56ee7490cd44068914eda4395bba1ce86d
hoapng <giahoa01giathieu@gmail.com>
2024-03-02 11:14:14 +0700

    hoàn thiện homecv

--- src/components/content/homecv.tsx
+++ src/components/content/homecv.tsx
@@ -1,34 +1,60 @@
+import { TypeAnimation } from "react-type-animation";
+
 const HomeCV = () => {
-    return (
-        <>
-            <div className="arlo_tm_section" id="home">
-                <div className="arlo_tm_hero_header_wrap">
-                    <div className="arlo_tm_universal_box_wrap">
-                        <div className="bg_wrap">
-                            <div className="overlay_image hero jarallax" data-speed="0.1"></div>
-                            <div className="overlay_color hero"></div>
-                        </div>
-                        <div className="content hero">
-                            <div className="inner_content">
-                                <div className="image_wrap">
-                                    <img src="img/hero/img.jpg" alt="hero" />
-                                </div>
-                                <div className="name_holder">
-                                    <h3>Alan <span>Michaelis</span></h3>
-                                </div>
-                                <div className="text_typing">
-                                    <p>I'm a <span className="arlo_tm_animation_text_word"></span></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="arlo_tm_arrow_wrap bounce anchor">
-                            <a href="#about"><i className="xcon-angle-double-down"></i></a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="arlo_tm_section" id="home">
+        <div className="arlo_tm_hero_header_wrap">
+          <div className="arlo_tm_universal_box_wrap">
+            <div className="bg_wrap">
+              <div
+                className="overlay_image hero jarallax"
+                data-speed="0.1"
+              ></div>
+              <div className="overlay_color hero"></div>
             </div>
-        </>
-    )
-}
+            <div className="content hero">
+              <div className="inner_content">
+                <div className="image_wrap">
+                  <img src="img/hero/hoa.png" alt="hero" />
+                </div>
+                <div className="name_holder">
+                  <h3>
+                    Phạm Nguyễn <span>Gia Hòa</span>
+                  </h3>
+                </div>
+                <div className="text_typing">
+                  <p>
+                    I'm a{/* <> </> */}
+                    &nbsp;
+                    <TypeAnimation
+                      sequence={[
+                        "Fullstack Web Developer",
+                        2000,
+
+                        () => {
+                          console.log("Sequence completed"); // Place optional callbacks anywhere in the array
+                        },
+                      ]}
+                      wrapper="span"
+                      cursor={true}
+                      repeat={Infinity}
+                      // style={{ fontSize: '2em', display: 'inline-block' }}
+                    />
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="arlo_tm_arrow_wrap bounce anchor">
+              <a href="#about">
+                <i className="xcon-angle-double-down"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomeCV;
