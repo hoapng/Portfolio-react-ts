ae4a218fd082efc9d60d6560f9b532664465881d
hoapng <giahoa01giathieu@gmail.com>
2024-03-04 10:36:52 +0700

    FloatButton

--- src/screens/portfolio.tsx
+++ src/screens/portfolio.tsx
@@ -2,42 +2,43 @@ import LeftPart from "../components/leftpart";
 import MobileMenu from "../components/mobile/menu";
 import PreLoader from "../components/preloader";
 import RightPart from "../components/rightpart";
+import { FloatButton } from "antd";
 
 const Portfolio = () => {
-    return (
-        <div className="arlo_tm_wrapper_all">
-
-            <div id="arlo_tm_popup_blog">
-                <div className="container">
-                    <div className="inner_popup scrollable"></div>
-                </div>
-                <span className="close"><a href="#"></a></span>
-            </div>
-
-            {/* <!-- PRELOADER --> */}
-            <PreLoader />
-            {/* <!-- /PRELOADER --> */}
-
-            {/* <!-- MOBILE MENU --> */}
-            <MobileMenu />
-            {/* <!-- /MOBILE MENU --> */}
-
-            {/* <!-- CONTENT --> */}
-            <div className="arlo_tm_content">
-
-                {/* <!-- LEFTPART --> */}
-                <LeftPart />
-                {/* <!-- /LEFTPART --> */}
-
-                {/* <!-- RIGHTPART --> */}
-                <RightPart />
-                {/* <!-- /RIGHTPART --> */}
-
-                <a className="arlo_tm_totop" href="#"></a>
-
-            </div>
+  return (
+    <div className="arlo_tm_wrapper_all">
+      <div id="arlo_tm_popup_blog">
+        <div className="container">
+          <div className="inner_popup scrollable"></div>
         </div>
-    )
-}
+        <span className="close">
+          <a href="#"></a>
+        </span>
+      </div>
+
+      {/* <!-- PRELOADER --> */}
+      <PreLoader />
+      {/* <!-- /PRELOADER --> */}
+
+      {/* <!-- MOBILE MENU --> */}
+      <MobileMenu />
+      {/* <!-- /MOBILE MENU --> */}
+
+      {/* <!-- CONTENT --> */}
+      <div className="arlo_tm_content">
+        {/* <!-- LEFTPART --> */}
+        <LeftPart />
+        {/* <!-- /LEFTPART --> */}
+
+        {/* <!-- RIGHTPART --> */}
+        <RightPart />
+        {/* <!-- /RIGHTPART --> */}
+
+        {/* <a className="arlo_tm_totop" href="#"></a> */}
+        <FloatButton.BackTop tooltip={<div>Scroll to Top</div>} />
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
